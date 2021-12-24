0628d3af1dcf694b1b77e765753045faa1af3429
Özgür Kürüm <kurumozgur@outlook.com>
2021-12-24 17:43:26 +0300

    Gün ve burç hesaplama eklendi.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,74 +40,93 @@ namespace WFATasarımOrnegi
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(136, 94);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(115, 89);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(163, 24);
+            this.label1.Size = new System.Drawing.Size(169, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Doğum Tarihiniz";
             // 
             // dtpDogumTarihi
             // 
-            this.dtpDogumTarihi.Location = new System.Drawing.Point(140, 133);
+            this.dtpDogumTarihi.CalendarMonthBackground = System.Drawing.SystemColors.InactiveCaption;
+            this.dtpDogumTarihi.CalendarTitleBackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.dtpDogumTarihi.CalendarTrailingForeColor = System.Drawing.Color.Gray;
+            this.dtpDogumTarihi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dtpDogumTarihi.Location = new System.Drawing.Point(120, 127);
+            this.dtpDogumTarihi.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.dtpDogumTarihi.Name = "dtpDogumTarihi";
-            this.dtpDogumTarihi.Size = new System.Drawing.Size(260, 28);
+            this.dtpDogumTarihi.Size = new System.Drawing.Size(280, 30);
             this.dtpDogumTarihi.TabIndex = 1;
             // 
             // btnHesapla
             // 
-            this.btnHesapla.Location = new System.Drawing.Point(140, 184);
+            this.btnHesapla.BackColor = System.Drawing.Color.Red;
+            this.btnHesapla.Location = new System.Drawing.Point(207, 340);
+            this.btnHesapla.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnHesapla.Name = "btnHesapla";
-            this.btnHesapla.Size = new System.Drawing.Size(127, 36);
+            this.btnHesapla.Size = new System.Drawing.Size(150, 41);
             this.btnHesapla.TabIndex = 2;
             this.btnHesapla.Text = "Hesapla";
-            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.UseVisualStyleBackColor = false;
             this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
             // 
             // lblYas
             // 
             this.lblYas.AutoSize = true;
-            this.lblYas.Location = new System.Drawing.Point(136, 234);
+            this.lblYas.BackColor = System.Drawing.Color.Transparent;
+            this.lblYas.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.lblYas.Location = new System.Drawing.Point(115, 194);
+            this.lblYas.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblYas.Name = "lblYas";
-            this.lblYas.Size = new System.Drawing.Size(82, 24);
+            this.lblYas.Size = new System.Drawing.Size(89, 25);
             this.lblYas.TabIndex = 3;
             this.lblYas.Text = "Yaşınız:";
             // 
             // lblYasadiginizGun
             // 
             this.lblYasadiginizGun.AutoSize = true;
-            this.lblYasadiginizGun.Location = new System.Drawing.Point(136, 267);
+            this.lblYasadiginizGun.BackColor = System.Drawing.Color.Transparent;
+            this.lblYasadiginizGun.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.lblYasadiginizGun.Location = new System.Drawing.Point(115, 233);
+            this.lblYasadiginizGun.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblYasadiginizGun.Name = "lblYasadiginizGun";
-            this.lblYasadiginizGun.Size = new System.Drawing.Size(167, 24);
+            this.lblYasadiginizGun.Size = new System.Drawing.Size(176, 25);
             this.lblYasadiginizGun.TabIndex = 3;
             this.lblYasadiginizGun.Text = "Yaşadığınız Gün:";
             // 
             // lblBurcunuz
             // 
             this.lblBurcunuz.AutoSize = true;
-            this.lblBurcunuz.Location = new System.Drawing.Point(136, 302);
+            this.lblBurcunuz.BackColor = System.Drawing.Color.Transparent;
+            this.lblBurcunuz.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.lblBurcunuz.Location = new System.Drawing.Point(115, 271);
+            this.lblBurcunuz.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblBurcunuz.Name = "lblBurcunuz";
-            this.lblBurcunuz.Size = new System.Drawing.Size(105, 24);
+            this.lblBurcunuz.Size = new System.Drawing.Size(110, 25);
             this.lblBurcunuz.TabIndex = 3;
             this.lblBurcunuz.Text = "Burcunuz:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(501, 415);
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(533, 479);
             this.Controls.Add(this.lblBurcunuz);
             this.Controls.Add(this.lblYasadiginizGun);
             this.Controls.Add(this.lblYas);
             this.Controls.Add(this.btnHesapla);
             this.Controls.Add(this.dtpDogumTarihi);
             this.Controls.Add(this.label1);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Burç Gün Yaş Hesapla";
+            this.Text = "Yaş Gün ve Burç Hesaplama";
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Form1.cs
+++ Form1.cs
@@ -20,8 +20,67 @@ namespace WFATasarımOrnegi
         private void btnHesapla_Click(object sender, EventArgs e)
         {
             DateTime dt = dtpDogumTarihi.Value;
+            TimeSpan gecenSure = DateTime.Now - dt;
+
             int yil = DateTime.Now.Year - dtpDogumTarihi.Value.Year;
-            lblYas.Text += yil.ToString();
+            lblYas.Text += " " + yil.ToString();
+            int toplamGün = (int)gecenSure.TotalDays;
+
+            lblYasadiginizGun.Text += " " + ((int)toplamGün).ToString();
+
+            int month = dt.Month;
+            int day = dt.Day;
+            string burc = "";
+            if ((month == 1 && day >= 21) || (month == 2 && day <= 19))
+            {
+                burc = "Kova";
+            }
+            else if ((month == 2 && day >= 20) || (month == 3 && day <= 20))
+            {
+                burc = "Balık";
+            }
+            else if ((month == 3 && day >= 21) || (month == 4 && day <= 20))
+            {
+                burc = "Koç";
+            }
+            else if ((month == 4 && day >= 21) || (month == 5 && day <= 21))
+            {
+                burc = "Boğa";
+            }
+            else if ((month == 5 && day >= 22) || (month == 6 && day <= 21))
+            {
+                burc = "İkizler";
+            }
+            else if ((month == 6 && day >= 22) || (month == 7 && day <= 23))
+            {
+                burc = "Yengeç";
+            }
+            else if ((month == 7 && day >= 24) || (month == 8 && day <= 23))
+            {
+                burc = "Aslan";
+            }
+            else if ((month == 8 && day >= 24) || (month == 9 && day <= 22))
+            {
+                burc = "Başak";
+            }
+            else if ((month == 9 && day >= 23) || (month == 10 && day <= 23))
+            {
+                burc = "Terazi";
+            }
+            else if ((month == 10 && day >= 24) || (month == 11 && day <= 22))
+            {
+                burc = "Akrep";
+            }
+            else if ((month == 11 && day >= 23) || (month == 12 && day <= 21))
+            {
+                burc = "Yay";
+            }
+            else
+            {
+                burc = "Oğlak";
+            }
+            lblBurcunuz.Text += " " + burc;
         }
-    }
+
+    }   
 }
